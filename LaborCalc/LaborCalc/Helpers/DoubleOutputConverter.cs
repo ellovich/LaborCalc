100a978b1ca8c9ac0f96068ed6f3a60dddc12085
Daniil Ellovich <49017080+ellovich@users.noreply.github.com>
2022-10-16 03:21:08 +0300

    Labor viewer fix

---
+++ LaborCalc/LaborCalc/Helpers/DoubleOutputConverter.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace LaborCalc.Helpers;
+
+public class DoubleOutputConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // For add this is simple. just return the sum of the value and the parameter.
+        // You may want to validate value and parameter in a real world App
+        return (double)Math.Round((double)value, 2);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // If we want to convert back, we need to subtract instead of add.
+        return (double)Math.Round((double)value, 2);
+    }
+}
